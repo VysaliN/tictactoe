b9035ec2e354ee03875a10d75a3ec8bf74c69683
Vysali Natarajan Jayaraman <vjnatarajan@AD.MSYSTECHNOLOGIES.COM>
2022-12-12 23:56:18 +0530

    first commit

---
+++ src/TicTacToe.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import "./TicTacToe.css";
+
+const TicTacToe = () => {
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState("");
+  const [loser, setLoser] = useState(true);
+
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      daigonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
+    }
+  };
+  const handleClick = (num) => {
+    let squares = [...cells];
+    if (cells[num] !== "") {
+      alert("already clicked");
+      return true;
+    }
+
+    if (turn === "X") {
+      squares[num] = "X";
+      setTurn("0");
+    } else {
+      squares[num] = "0";
+      setTurn("X");
+    }
+    if (squares.includes("") === false) {
+      setLoser("Game Over");
+    }
+    if (winner.length === 1) {
+      alert(`${winner} is the winner`);
+      return true;
+    }
+    checkForWinner(squares);
+    setCells(squares);
+  };
+  console.log(loser);
+  console.log(winner.length);
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+  };
+  return (
+    <div className="tictoctoe">
+      <div className="tic">
+        <h1>Tic Tac Toe</h1>
+        <div className="container">
+          <table>
+            <tbody>
+              <tr>
+                <Cell num={0} />
+                <Cell num={1} />
+                <Cell num={2} />
+              </tr>
+              <tr>
+                <Cell num={3} />
+                <Cell num={4} />
+                <Cell num={5} />
+              </tr>
+              <tr>
+                <Cell num={6} />
+                <Cell num={7} />
+                <Cell num={8} />
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <h2>Turn: {turn}</h2>
+        {winner
+          ? winner && (
+              <>
+                <p className="winner">{winner} is the winner</p>
+                <button
+                  onClick={() => [setCells(Array(9).fill("")), setWinner("")]}
+                >
+                  Play Again
+                </button>
+              </>
+            )
+          : loser === "Game Over" && (
+              <>
+                <p className="loser">{loser}</p>
+                <button
+                  onClick={() => [setCells(Array(9).fill("")), setLoser("")]}
+                >
+                  Play Again
+                </button>
+              </>
+            )}
+      </div>
+    </div>
+  );
+};
+
+export default TicTacToe;
